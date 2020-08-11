c86e689dd7496bef424bacb1b104da8ba7ca4c49
psjishnu <psjishnu13@gmail.com>
2020-08-11 10:46:58 +0530

    Added scroll progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HomePage from "./components/HomePage";
 import "../src/styles/main_style.css";
-function App() {
+import Progress from "./components/Progress";
+
+const App = () => {
+  const [scrollPostion, setscrollPostion] = useState(0);
+
+  const calculateScrollDistance = () => {
+    const scrollTop = window.pageYOffset;
+    const winHeight = window.innerHeight;
+    const docHeight = getDocHeight();
+    const totalDocScrollLength = docHeight - winHeight;
+    const Position = Math.floor((scrollTop / totalDocScrollLength) * 100);
+    setscrollPostion(Position);
+  };
+
+  const getDocHeight = () => {
+    return Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight,
+      document.body.clientHeight,
+      document.documentElement.clientHeight
+    );
+  };
+
+  const listenToScrollEvent = () => {
+    document.addEventListener("scroll", () => {
+      requestAnimationFrame(() => {
+        calculateScrollDistance();
+      });
+    });
+  };
+
+  useEffect(() => {
+    listenToScrollEvent();
+  }, []);
+
   return (
     <div className="App">
+      <Progress scroll={scrollPostion + "%"} />
       <HomePage />
     </div>
   );
-}
+};
 
 export default App;
